11dcb15b38aad036da420c9397bb9e2ad79cf9fa
martin <martin@azumo.co>
2019-04-15 05:11:38 -0300

    1 - TDD

--- test/martin/stringCalculator.test.js
+++ test/martin/stringCalculator.test.js
@@ -1,5 +1,5 @@
 const assert = require("chai").assert;
-const StringCalculator = require("../1 - TDD/stringCalculator");
+const StringCalculator = require("./stringCalculator");
 
 describe("String Calculator", () => {
     it('empty string returns 0', () => {
