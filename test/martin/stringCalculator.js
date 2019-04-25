4d92b07f5661ac4c2c18c42a42f2d5b7ac7239c6
Nahuel Rullo <nahuelrullo@gmail.com>
2019-04-25 11:33:33 -0300

    Revert "1-4 steps"

--- test/martin/stringCalculator.js
+++ test/martin/stringCalculator.js
@@ -1,25 +1,6 @@
 class StringCalculator {
-    add(values) {
-        const inputDelimiter = this.getInputDelimitar(values);
-        const numbers = this.parseAndSplitToNumbers(values, inputDelimiter);
-        let sum = 0;
-        numbers.forEach(number => {
-            sum += Number(number);
-        });
-        return sum;
-    }
-
-    parseAndSplitToNumbers(values, customDelimiter) {
-        if (customDelimiter !== "") {
-            values = values.replace(/\/\//g, ",").replace(new RegExp(customDelimiter, "g"), ",");
-        }
-        return values.replace(/\n/g, ",").split(",");
-    }
-
-    getInputDelimitar(values) {
-        let re = new RegExp("^\/\/(.*?)\\n.*", "g");
-        let match = re.exec(values);
-        return match && match.length > 1? match[1] : "";
+    add(values){
+        return -1;
     }
 }
 
