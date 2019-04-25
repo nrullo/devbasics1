339b25bf6de7f62a02cba4742636ffa3db2c8ca3
nrullo <nahuelrullo@gmail.com>
2019-04-25 09:40:50 -0300

    Added tests cases when input is an unknown amount of numbers

--- test/martin/stringCalculator.test.js
+++ test/martin/stringCalculator.test.js
@@ -25,4 +25,24 @@ describe("String Calculator", () => {
     it('input \"99,1\" returns 100', () => {
         assert.equal(StringCalculator.add("99,1"), 100);
     });
+
+    it('input \",,,,,,,,,,\" returns 0', () => {
+        assert.equal(StringCalculator.add(",,,,,,,,,,"), 0);
+    });
+
+    it('input \"0,0,0,0,0,0,0,0\" returns 0', () => {
+        assert.equal(StringCalculator.add("0,0,0,0,0,0,0,0"), 0);
+    });
+
+    it('input \"1,2,3\" returns 6', () => {
+        assert.equal(StringCalculator.add("1,2,3"), 6);
+    });
+
+    it('input \"0,1,2,3,4,5,6,7,8,9\" returns 45', () => {
+        assert.equal(StringCalculator.add("0,1,2,3,4,5,6,7,8,9"), 45);
+    });
+
+    it('input \"1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\" returns 15', () => {
+        assert.equal(StringCalculator.add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"), 15);
+    });
 });
