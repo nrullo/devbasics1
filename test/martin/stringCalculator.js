2c24d499cfa20b839120f00ebe540a1472586590
nrullo <nahuelrullo@gmail.com>
2019-04-25 11:15:50 -0300

    Support different delimiters

--- test/martin/stringCalculator.js
+++ test/martin/stringCalculator.js
@@ -1,6 +1,7 @@
 class StringCalculator {
     add(values) {
-        const numbers = this.parseAndSplitToNumbers(values);
+        const inputDelimiter = this.getInputDelimitar(values);
+        const numbers = this.parseAndSplitToNumbers(values, inputDelimiter);
         let sum = 0;
         numbers.forEach(number => {
             sum += Number(number);
@@ -8,8 +9,17 @@ class StringCalculator {
         return sum;
     }
 
-    parseAndSplitToNumbers(values) {
+    parseAndSplitToNumbers(values, customDelimiter) {
+        if (customDelimiter !== "") {
+            values = values.replace(/\/\//g, ",").replace(new RegExp(customDelimiter, "g"), ",");
+        }
         return values.replace(/\n/g, ",").split(",");
+    }
+
+    getInputDelimitar(values) {
+        let re = new RegExp("^\/\/(.*?)\\n.*", "g");
+        let match = re.exec(values);
+        return match && match.length > 1? match[1] : "";
     }
 }
 
--- test/martin/stringCalculator.test.js
+++ test/martin/stringCalculator.test.js
@@ -46,15 +46,27 @@ describe("String Calculator", () => {
         assert.equal(StringCalculator.add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"), 15);
     });
 
-    it('input \"1\\n2,3\" returns 6', () => {
+    it('input \"1\n2,3\" returns 6', () => {
         assert.equal(StringCalculator.add("1\n2,3"), 6);
     });
 
-    it('input \"1\\n21,3\" returns 25', () => {
+    it('input \"1\n21,3\" returns 25', () => {
         assert.equal(StringCalculator.add("1\n21,3"), 25);
     });
 
-    it('input \"0,1\\n2,3,4\n5,6,7,8\n9\" returns 45', () => {
+    it('input \"0,1\n2,3,4\n5,6,7,8\n9\" returns 45', () => {
         assert.equal(StringCalculator.add("0,1\n2,3,4\n5,6,7,8\n9"), 45);
+    });
+
+    it('input \"//;\n1;2\" returns 3', () => {
+        assert.equal(StringCalculator.add("//;\n1;2"), 3);
+    });
+
+    it('input \"//X\n1X2,4\" returns 7', () => {
+        assert.equal(StringCalculator.add("//X\n1X2,4"), 7);
+    });
+
+    it('input \"//SEP\n1SEP2SEP20,30\" returns 53', () => {
+        assert.equal(StringCalculator.add("//SEP\n1SEP2SEP20,30"), 53);
     });
 });
