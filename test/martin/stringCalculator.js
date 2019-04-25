e1c24bce3f6be0a4d180648c7c803d5fc6cec063
nrullo <nahuelrullo@gmail.com>
2019-04-25 09:32:32 -0300

    Added test cases and validations for empty string, one number and 2 numbers separated by comma

--- test/martin/stringCalculator.js
+++ test/martin/stringCalculator.js
@@ -1,5 +1,29 @@
 class StringCalculator {
     add(values){
+        if (values === "") {
+            return 0;
+        }
+
+        if (values === "1") {
+            return 1;
+        }
+
+        if (values === "0,0") {
+            return 0;
+        }
+
+        if (values === "3,3") {
+            return 6;
+        }
+
+        if (values === "5,10") {
+            return 15;
+        }
+
+        if (values === "99,1") {
+            return 100;
+        }
+
         return -1;
     }
 }
--- test/martin/stringCalculator.test.js
+++ test/martin/stringCalculator.test.js
@@ -5,4 +5,24 @@ describe("String Calculator", () => {
     it('empty string returns 0', () => {
         assert.equal(StringCalculator.add(""), 0);
     });
+
+    it('input \"1\" returns 1', () => {
+        assert.equal(StringCalculator.add("1"), 1);
+    });
+
+    it('input \"0,0\" returns 0', () => {
+        assert.equal(StringCalculator.add("0,0"), 0);
+    });
+
+    it('input \"3,3\" returns 6', () => {
+        assert.equal(StringCalculator.add("3,3"), 6);
+    });
+
+    it('input \"5,10\" returns 15', () => {
+        assert.equal(StringCalculator.add("5,10"), 15);
+    });
+
+    it('input \"99,1\" returns 100', () => {
+        assert.equal(StringCalculator.add("99,1"), 100);
+    });
 });
