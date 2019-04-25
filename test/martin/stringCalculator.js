c7ee0b88f9907eeeca33aa6d53cc04750cd2d222
nrullo <nahuelrullo@gmail.com>
2019-04-25 09:36:32 -0300

    Refactor for test cases and validations for empty string, one number and 2 numbers separated by comma

--- test/martin/stringCalculator.js
+++ test/martin/stringCalculator.js
@@ -1,30 +1,11 @@
 class StringCalculator {
     add(values){
-        if (values === "") {
-            return 0;
-        }
-
-        if (values === "1") {
-            return 1;
-        }
-
-        if (values === "0,0") {
-            return 0;
-        }
-
-        if (values === "3,3") {
-            return 6;
-        }
-
-        if (values === "5,10") {
-            return 15;
-        }
-
-        if (values === "99,1") {
-            return 100;
-        }
-
-        return -1;
+        const numbers = values.split(",");
+        let sum = 0;
+        numbers.forEach(number => {
+            sum += Number(number);
+        });
+        return sum;
     }
 }
 
